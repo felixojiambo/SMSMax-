7e7a9203f9b5f0128cf61c9b9b607c7eb2ca7d10
felixojiambo <felixojiamboe@gmail.com>
2024-10-04 23:01:14 +0300

    Retry Configuration

---
+++ src/main/java/com/design/SMSMax/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.design.SMSMax.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+@Configuration
+@EnableRetry
+public class RetryConfig {
+    // Default configuration or custom configurations
+}
