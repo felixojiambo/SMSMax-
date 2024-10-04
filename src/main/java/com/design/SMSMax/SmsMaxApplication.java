7ca830d3c015032e384379154a8e22614fddd811
felixojiambo <felixojiamboe@gmail.com>
2024-10-04 22:24:59 +0300

    initial commit

---
+++ src/main/java/com/design/SMSMax/SmsMaxApplication.java
@@ -0,0 +1,13 @@
+package com.design.SMSMax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/design/SMSMax/SmsMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.SMSMax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
